ba6a815df9dfd7eaae2c06c51bbdf9f1f6f34a23
Lemonatix <ph.mika.ma.riesterer@gmail.com>
2024-11-13 02:41:48 +0100

    adds python loops

---
+++ Loop Statements/for loop.py
@@ -0,0 +1,4 @@
+list_1 = ["apple", "banana", "cherry"]
+
+for item in list_1: # since we refer to list_1 in the for loop: pyhon knows, that one item of the list is one index (here one string)
+    print(item)
---
+++ Loop Statements/while loop.py
@@ -0,0 +1,6 @@
+list_1 = ["apple", "banana", "cherry"]
+
+index = 0 
+while index < len(list_1): # since index = 0 which is smaller then the length of the list, the whole list gets printed
+    print(list_1[index])
+    index += 1
