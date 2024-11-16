43ef54088a40abf22e5833e0cb570bebe5512ff3
Lemonatix <ph.mika.ma.riesterer@gmail.com>
2024-11-16 01:49:55 +0100

    Update indentation.py

--- indentation.py
+++ indentation.py
@@ -1,10 +1,6 @@
 def myFunction():
     print("hello")
-    print("hello world") # this is correct
-
-def myFunction():
-    print("hello")
-     print("hello world") # this is not correct and the interpreter is confused about the different indentation
+    print("hello world") # this is correct, if both prints have different identation, an error occures
     
 def myFunction():
   print('hello')
@@ -14,11 +10,9 @@ def myOtherFunction():
           print(1)
           print(2) # multiple indentations are possible 
 
-
 a=2
 
 if a == 2:
     print("a is equal to 2")
     if a % 2 == 0:
         print("a is even") # 2 Statements, both in an if argument
-
