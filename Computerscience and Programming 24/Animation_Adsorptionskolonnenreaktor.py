1c542bf841441fc91ca059ef939023431a2895af
Lemonatix <ph.mika.ma.riesterer@gmail.com>
2024-11-22 17:21:43 +0100

    adds some commets regarding the animation

--- Computerscience and Programming 24/Animation_Adsorptionskolonnenreaktor.py
+++ Computerscience and Programming 24/Animation_Adsorptionskolonnenreaktor.py
@@ -4,13 +4,13 @@ from matplotlib.animation import FuncAnimation
 
 # Initialisierung der Matrix
 concentration = np.zeros([100, 100])
-concentration[0, :] = np.ones(100)
+concentration[0, :] = 1
 temp_mat = np.zeros([100, 100])
 
 # Daten für Animation sammeln
 frames = []
 
-for steps in range(50):
+for steps in range(50): # Anzahl der iterierten Diffusionsschritte
     for i in range(100):
         for j in range(100):
             temp = 0
@@ -24,17 +24,17 @@ for steps in range(50):
                 temp += concentration[i, j+1]
             temp_mat[i, j] = 0.25 * temp
     concentration = temp_mat
-    concentration[0, :] = np.ones(100)
-    frames.append(concentration.copy())
+    concentration[0, :] = 1
+    frames.append(concentration.copy()) # 1 Frame = 1 Step
 
 # Animation erstellen
-fig, ax = plt.subplots()
-im = ax.imshow(frames[0][1:, :], cmap='viridis', aspect='auto')
+fig, ax = plt.subplots() # erstellt subplots
+im = ax.imshow(frames[0][1:, :], cmap='viridis', aspect='auto') # erstellt Bild 
 
-def update(frame):
+def update(frame): # aktualisiert die Bilder bzgl. den gesammelten frames (Liste)
     im.set_data(frame[1:, :])
-    return [im]
+    return[im]
 
-ani = FuncAnimation(fig, update, frames=frames, interval=100, blit=True)
+ani = FuncAnimation(fig, update, frames=frames, interval=100, blit=True) # erstellt Animation, bzw. Bilder werden hintereinander abgespielt
 plt.colorbar(im, ax=ax, label="Concentration")
 plt.show()
