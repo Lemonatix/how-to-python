909ea788e916fcdb8b8f5be8ceccf304c473af6c
Lemonatix <ph.mika.ma.riesterer@gmail.com>
2024-12-13 14:38:21 +0100

    adds working myserv

--- myserv.py
+++ myserv.py
@@ -1,6 +1,7 @@
 from typing import Union
 import uvicorn
 from fastapi import FastAPI
+import pickle
 
 app = FastAPI()
 
@@ -8,14 +9,31 @@ app = FastAPI()
 def read_root():
     return {"Eine Nachricht": "Hallo Welt!"}
 
-
 @app.get("/rechnung/{a}-sowie-{b}")
 def read_item(a: int, b: int):
+    pickle.dump(a, open("saved_number.pck", "wb"))
     return {"Gesendet wurden": str(a) + " und " + str(b) , "Die Summe ist": a+b}
 
-#if __name__ == "__main__":
-#    uvicorn.run(app, host="192.168.0.1", port=1337)
+@app.get("/multiplikation/{a}-mal-{b}")
+def multiply(a: int, b: int):
+    return {"Gesendet wurden": str(a) + " und " + str(b), "Das Produkt ist": a * b}
+
+@app.get("/lade/")
+def laden():
+    # datei laden
+    a = pickle.load(open("saved_number.pck", "rb"))
+    return{"Gelesen wurde: ": str(a)}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="localhost", port=1337)
+
 # ipv4: 127.0.0.1
 # 0.0.0.0 ist local host
 # standard-port ist 80
 # xyz.de aufgerufen -> DNS (Domain-Name-System) aufgerufen -> 126.183.92.23:80
+
+'''commands für 
+cd .. -> 1 directory zurück
+mkdir -> directory erstellen
+ls / cd zeigt alle möglichen Verzeichnisse
+'''
