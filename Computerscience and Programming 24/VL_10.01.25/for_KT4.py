e5bda090c30d4667f66ecd95d8c6b9bf9a3434aa
Lemonatix <ph.mika.ma.riesterer@gmail.com>
2025-01-11 01:51:49 +0100

    small adjustments

--- Computerscience and Programming 24/VL_10.01.25/for_KT4.py
+++ Computerscience and Programming 24/VL_10.01.25/for_KT4.py
@@ -1,3 +1,9 @@
+# Mittelwert
+# Median
+# Varianz ausrechnen
+# from scratch implementierung von mittelwert
+# funktionsdefinitionen & argsort
+
 import numpy as np
 x = [3, 4, 8, 9]
 for element in x:
