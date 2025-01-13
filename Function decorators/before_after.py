54d1bd4887332a0b4729b46dec395693579e6bbf
Lemonatix <ph.mika.ma.riesterer@gmail.com>
2025-01-13 21:00:19 +0100

    adds another example

--- Function decorators/before_after.py
+++ Function decorators/before_after.py
@@ -5,8 +5,22 @@ def before_after_decorator(func):
         print("After")
     return wrapper
 
-@before_after_decorator 
+@before_after_decorator
 def say_hello(name):
     print(f"Hello {name}")
 
 say_hello("John")
+
+# ---------------------
+def my_decorator(func):
+    def wrapper():
+        print("Es passiert etwas vor dem Funktionsaufruf.")
+        func()
+        print("Es passiert etwas danach.")
+    return wrapper
+
+@my_decorator
+def sag_hallo():
+    print("Servus!")
+
+sag_hallo()
