511c365d4f794a07be432eb0fc71c8e53aefe996
Lemonatix <ph.mika.ma.riesterer@gmail.com>
2025-01-14 17:38:53 +0100

    adds solution to KT4

---
+++ KT4 prep/KT4 Solution.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+arr1 = np.random.randint(0, 100, size=(5, 5))
+
+def calculation(arr):
+    arr = arr.flatten()  # Ergebnis speichern
+    m = sum(arr) / len(arr)  # Mittelwert
+    v = sum((x - m) ** 2 for x in arr) / (len(arr) - 1)  # Varianz
+    dict = {"Mittelwert": m, "Varianz": v}
+    return m, v, dict # dict funktioniert hier, ist allgemein aber problematisch
+
+print(calculation(arr1))
